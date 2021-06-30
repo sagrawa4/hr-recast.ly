eaaf01a2c7e227f361e5184429393a4eef0bd9ba
Shruti Agrawal <shruti98932@gmail.com>
2021-06-30 11:11:01 -0400

    VideoPlayer, VideoList, VideoListEntry Implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,46 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      video: exampleVideoData[0],
+      videos: exampleVideoData
+    };
+  }
+
+  onListItemClick(video) {
+    this.setState({
+      video: video
+    });
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em> view goes here</h5></div>
+            <VideoPlayer video={this.state.video} />
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> view goes here</h5></div>
+            <VideoList myevent={this.onListItemClick.bind(this)}videos={this.state.videos} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,49 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
 
+var VideoList = (props) => {
+  return (
+    <div className='video-list'>
+      {props.videos.map(video => {
+        return (<VideoListEntry click={props.myevent} video={video} key={video.id.videoId} />);
+      }
+      )}
+
+      {/* we have to make this dynamic  */}
+      {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
+    </div>
+  );
+
+};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
+
+// console.log("V" , VideoList.propTypes )
+// VideoList.videoRender = {
+//   for( var i=0; i< exampleVideoData.length; i++) {
+//     ReactDOM.render(exampleVideoData[i], document.getElementById('video-list-entry media'));
+//   }
+// }
+// I think this is giving us individual video data, but this needs to be in HTML before  putting on the dom. We need to:
+// 1. get the video data
+// 2. turn it into HTML aka into videoListEntry
+//  ..... how do we wanna output the entry?
+//  ..... Map over a component that has all the videos
+//  ..... similar to groceryList
+//  return (
+//   <ul>
+//   {props.groceries.map(list =>
+//     <GroceryListItem list={list} />
+//   )}
+// </ul>
+// )
+// 3. render it onto the page
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
